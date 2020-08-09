f36d0a27688562028be02876f07b2bd04e11eb8c
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-09 13:29:47 +0900

    add-routes-signup-and-controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
   resources :items, only: [:index, :show]
-  
+
+  resources :signup, only: [:create] do
+    collection do 
+      get 'new'
+      get 'user_registration1'
+      get 'user_registration2'
+      get 'user_registration3'
+      get 'user_registration4'
+      get 'done'
+    end
+  end
 end
