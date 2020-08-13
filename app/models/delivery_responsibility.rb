6cc0a558b2cfbea4dd96ca1088828ae372c25217
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-13 12:26:52 +0900

    commmit

--- app/models/delivery_responsibility.rb
+++ app/models/delivery_responsibility.rb
@@ -1,4 +1,4 @@
 class DeliveryResponsibility < ApplicationRecord
-  # validates :delivery_responsibility, presence: true
-  # has_many :items
+  validates :delivery_responsibility, presence: true
+  has_many :items
 end
