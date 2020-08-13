1c4519a4f9fac4a9a032e9d6675f7e322304b6d3
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-13 14:02:13 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_item, except: :index 
+  before_action :authenticate_user! except: :show
+  before_action :set_item, except: [:index, :new, :create]
+  
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :items, only: [:index, :show]
   resources :signup, only: [:create] do
     collection do 
       get 'new'
