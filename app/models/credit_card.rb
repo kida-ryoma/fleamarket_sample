954dfeb36f0b7283cdf02a1732a5b74a39e35348
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-13 12:54:27 +0900

    commit

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,6 +1,6 @@
 class CreditCard < ApplicationRecord
-  validates :customer_id, presence: true
-  validates :card_id, precence: true
+  # validates :customer_id, presence: true
+  # validates :card_id, precence: true
 
   # belongs_to :user, optional: true
 end
