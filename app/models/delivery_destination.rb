08a6be59a997cda7a4767197333078e92c871bb3
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-13 12:24:28 +0900

    commit

--- app/models/delivery_destination.rb
+++ app/models/delivery_destination.rb
@@ -1,12 +1,12 @@
 class DeliveryDestination < ApplicationRecord
-  # validates :delivery_family_name, presence: true
-  # validates :delivery_first_name, presence: true
-  # validates :delivery_family_name_kana, presence: true
-  # validates :delivery_first_name_kana, presence: true
-  # validates :post_code, presence: true
-  # validates :prefecture_code, presence: true
-  # validates :city, presence: true
-  # validates :house_number, presence: true
+  validates :delivery_family_name, presence: true
+  validates :delivery_first_name, presence: true
+  validates :delivery_family_name_kana, presence: true
+  validates :delivery_first_name_kana, presence: true
+  validates :post_code, presence: true
+  validates :prefecture_code, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
   
-  # belongs_to :user, optional: true
+  belongs_to :user, optional: true
 end
