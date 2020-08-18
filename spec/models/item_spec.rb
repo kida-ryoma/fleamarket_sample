f2c5201106034684e094f91ddc197b1e9daa07c5
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-18 14:31:17 +0900

    commit-model-test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -48,7 +48,47 @@ require 'rails_helper'
         preparation_day = create(:preparation_day)
         item = build(:item, status_id: nil,  category_id: category[:id], user_id: user[:id], delivery_responsibility_id: delivery_responsibility[:id], preparation_day_id: preparation_day[:id] )
         item.valid?
-        expect(item.errors[:status_id]).to include("can't be blank")
+        expect(item.errors.full_messages).to include("Status must exist")
+      end
+      it "is invalid without a category_id" do
+        category = create(:category)
+        status = create(:status)
+        delivery_responsibility = create(:delivery_responsibility)
+        user = create(:user)
+        preparation_day = create(:preparation_day)
+        item = build(:item, category_id: nil,  status_id: status[:id], user_id: user[:id], delivery_responsibility_id: delivery_responsibility[:id], preparation_day_id: preparation_day[:id] )
+        item.valid?
+        expect(item.errors.full_messages).to include("Category must exist")
+      end
+      it "is invalid without a user_id" do
+        category = create(:category)
+        status = create(:status)
+        delivery_responsibility = create(:delivery_responsibility)
+        user = create(:user)
+        preparation_day = create(:preparation_day)
+        item = build(:item, user_id: nil,  category_id: category[:id], status_id: status[:id], delivery_responsibility_id: delivery_responsibility[:id], preparation_day_id: preparation_day[:id] )
+        item.valid?
+        expect(item.errors.full_messages).to include("User must exist")
+      end
+      it "is invalid without a delivery_responsibility_id" do
+        category = create(:category)
+        status = create(:status)
+        delivery_responsibility = create(:delivery_responsibility)
+        user = create(:user)
+        preparation_day = create(:preparation_day)
+        item = build(:item, delivery_responsibility_id: nil,  category_id: category[:id], user_id: user[:id], status_id: status[:id], preparation_day_id: preparation_day[:id] )
+        item.valid?
+        expect(item.errors.full_messages).to include("Delivery responsibility must exist")
+      end
+      it "is invalid without a preparation_day_id" do
+        category = create(:category)
+        status = create(:status)
+        delivery_responsibility = create(:delivery_responsibility)
+        user = create(:user)
+        preparation_day = create(:preparation_day)
+        item = build(:item, preparation_day_id: nil,  category_id: category[:id], user_id: user[:id], delivery_responsibility_id: delivery_responsibility[:id], status_id: status[:id] )
+        item.valid?
+        expect(item.errors.full_messages).to include("Preparation day must exist")
       end
   end
 end
