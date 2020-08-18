6834b987402dcc34316657c9ab2a4b291eda6a2c
wattyo0314 <wantyan-wattyo.0314@outlook.jp>
2020-08-19 02:12:59 +0900

    finshed payjp
    クレジットカードも解決しました。

--- app/javascript/payjp.js
+++ app/javascript/payjp.js
@@ -1,16 +1,16 @@
 $(function() {
   $('#cardCreateForm').on('submit', function(e) {
     e.preventDefault()
-    Payjp.setPublicKey('pk_test_cf241efe47cd23679b76022d');
+    Payjp.setPublicKey('pk_test_cf241efe47cd23679b76022d')
     var card = {
       number: document.getElementById("card-number").value,
       exp_month: document.getElementById("credit_exp_month").value,
       exp_year: document.getElementById("credit_exp_year").value,
       cvc: document.getElementById("cvc").value
     };
-    console.log(card)
     if (card.number == "" || card.cvc == "") {
       alert("入力もれがあります");
+      $(".creditCardBtn").prop('disabled', false);
     } else {
       Payjp.createToken(card, function(status, response) {
         if (status === 200 ) {
@@ -25,6 +25,7 @@ $(function() {
           alert("登録に成功しました");
         } else {
           alert("カード情報が正しくありません");
+          $('.creditCardBtn').prop("disabled", false);
         }
       });
     }
