3a9c804bf85a32274f98655b652bb912beb265e8
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-21 15:56:42 +0900

    commit-change-item.price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
-  # ログイン機能未実装なのでコメントアウト中
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_user
 
   def show
     @user = User.find(params[:id])
+    @user_items = @user.items
   end
 
   private
   def set_user
     @user = User.find(params[:id])
   end
+  
 end
