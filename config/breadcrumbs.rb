36bdfc9182afbb804039af8770ec6e6ced24a263
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-08-24 19:29:02 +0900

    commit-bread-credit-card-my-page

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,12 @@ crumb :user do
   link "マイページ", user_path(current_user.id)
 end
 
+crumb :credit_card_delete do
+  link "クレジットカード削除", credit_card_path(:id)
+  parent :user
+end
+
+crumb :credit_card do
+  link "クレジットカード登録", new_credit_card_path
+  parent :user
+end
