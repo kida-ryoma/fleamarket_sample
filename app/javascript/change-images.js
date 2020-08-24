bd523b75dfffa766f72e307c9a6663d9f586c195
mameco <mayu.vwx@gmail.com>
2020-08-24 11:43:43 +0900

    edit show view

--- app/javascript/change-images.js
+++ app/javascript/change-images.js
@@ -1,9 +1,11 @@
-$(function (){
-  $(".smallImage").first().addClass("active");
-  $(".smallImage").click(function(){
-    image_url = $(this).attr("src");
-    $(".bigImage").attr("src", image_url).hide().fadeIn();
-    $(".smallImage.active").removeClass(".active");
-    $(this).addClass("active");
+$(document).on('turbolinks:load', function() { 
+  $(function (){
+    $(".smallImage").first().addClass("active");
+    $(".smallImage").click(function(){
+      image_url = $(this).attr("src");
+      $(".bigImage").attr("src", image_url).hide().fadeIn();
+      $(".smallImage.active").removeClass(".active");
+      $(this).addClass("active");
+    });
   });
 });
