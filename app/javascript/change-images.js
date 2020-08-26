fd9d0a9bfa8cf5e763c35167d9b762c8e9899f04
mameco <mayu.vwx@gmail.com>
2020-08-26 20:01:07 +0900

    remove turbolinks

--- app/javascript/change-images.js
+++ app/javascript/change-images.js
@@ -1,11 +1,9 @@
-$(document).on('turbolinks:load', function() { 
-  $(function (){
-    $(".smallImage").first().addClass("active");
-    $(".smallImage").click(function(){
-      image_url = $(this).attr("src");
-      $(".bigImage").attr("src", image_url).hide().fadeIn();
-      $(".smallImage.active").removeClass(".active");
-      $(this).addClass("active");
-    });
+$(function (){
+  $(".smallImage").first().addClass("active");
+  $(".smallImage").click(function(){
+    image_url = $(this).attr("src");
+    $(".bigImage").attr("src", image_url).hide().fadeIn();
+    $(".smallImage.active").removeClass(".active");
+    $(this).addClass("active");
   });
 });
