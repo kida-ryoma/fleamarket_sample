d1553c0fc87086f528f75a02ac11c236c349cc88
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-08-27 18:24:01 +0900

    クレジットカード無い場合の購入画面遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,29 +5,33 @@ class OrdersController < ApplicationController
   require"payjp"
 
   def new
-    Order.new
-    @address = DeliveryDestination.find_by(user_id: current_user.id)
-    @user = current_user
-    Payjp.api_key = ENV["SECRET_KEY"]
-      customer = Payjp::Customer.retrieve(@credit_card.customer_id)
-      @card_info = customer.cards.retrieve(customer.default_card)
-      @card_brand = @card_info.brand.to_s
-      @exp_month = @card_info.exp_month.to_s
-      @exp_year = @card_info.exp_year.to_s.slice(2,3)
-      case @card_brand
-      when "Visa" then
-        @card_image = "Visa.gif"
-      when "JCB" then
-        @card_image = "JCB.png"
-      when "MasterCard" then
-        @card_image = "MasterCard.png"
-      when "American Express" then
-        @card_image = "AmericanExpress.png"
-      when "Diners Club" then
-        @card_image = "DinersClub.png"
-      when "Discover" then
-        @card_image = "Discover.gif"
-      end
+    if @credit_card.present?
+      Order.new
+      @address = DeliveryDestination.find_by(user_id: current_user.id)
+      @user = current_user
+      Payjp.api_key = ENV["SECRET_KEY"]
+        customer = Payjp::Customer.retrieve(@credit_card.customer_id)
+        @card_info = customer.cards.retrieve(customer.default_card)
+        @card_brand = @card_info.brand.to_s
+        @exp_month = @card_info.exp_month.to_s
+        @exp_year = @card_info.exp_year.to_s.slice(2,3)
+        case @card_brand
+        when "Visa" then
+          @card_image = "Visa.gif"
+        when "JCB" then
+          @card_image = "JCB.png"
+        when "MasterCard" then
+          @card_image = "MasterCard.png"
+        when "American Express" then
+          @card_image = "AmericanExpress.png"
+        when "Diners Club" then
+          @card_image = "DinersClub.png"
+        when "Discover" then
+          @card_image = "Discover.gif"
+        end
+    else
+      redirect_to new_credit_card_path
+    end
   end
 
   def create
