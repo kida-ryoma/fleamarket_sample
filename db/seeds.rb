42dc5d4512cff96d5555630ea5ca80e488d37ac7
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-08-27 11:48:22 +0900

    delete unneeded dupulicate stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -135,7 +135,7 @@ interior_residence_accessory_13.children.create([{name: "すべて"}])
 # 本・音楽・ゲーム
 book_music_game = Category.create(name: "本・音楽・ゲーム")
 book_music_game_1 = book_music_game.children.create(name: "本")
-book_music_game_1.children.create([{name: "文学/小説"},{name: "人文/社会"},{name: "ノンフィクション/教養"},{name: "地図/旅行ガイド"},{name: "ビジネス/経済"},{name: "ビジネス/経済"},{name: "健康/医学"},{name: "コンピュータ/IT"},{name: "趣味/スポーツ/実用"},{name: "住まい/暮らし/子育て"},{name: "アート/エンタメ"},{name: "洋書"},{name: "絵本"},{name: "参考書"},{name: "その他"}])
+book_music_game_1.children.create([{name: "文学/小説"},{name: "人文/社会"},{name: "ノンフィクション/教養"},{name: "地図/旅行ガイド"},{name: "ビジネス/経済"},{name: "健康/医学"},{name: "コンピュータ/IT"},{name: "趣味/スポーツ/実用"},{name: "住まい/暮らし/子育て"},{name: "アート/エンタメ"},{name: "洋書"},{name: "絵本"},{name: "参考書"},{name: "その他"}])
 book_music_game_2 = book_music_game.children.create(name: "漫画")
 book_music_game_2.children.create([{name: "全巻セット"},{name: "少年漫画"},{name: "少女漫画"},{name: "青年漫画"},{name: "女性漫画"},{name: "同人誌"},{name: "その他"}])
 book_music_game_3 = book_music_game.children.create(name: "雑誌")
